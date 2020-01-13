4f4dd7f4ed240d19b595e319e7b9a78ba867aba3
Your Name <you@example.com>
2020-01-13 23:51:28 +0100

    udev correction and setup dialout for user

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
-sudo apt-get install expect
-sudo apt install picocom
-sudo apt install gcc-msp430 mspdebug
+yes | sudo apt-get install expect
+yes | sudo apt install picocom
+yes | sudo apt install gcc-msp430 mspdebug
 chmod +x *.sh
 chmod +x *.exp
-
+sudo adduser $USER dialout
+sudo cp .46-TI_launchpad.rules /etc/udev/rules.d/46-TI_launchpad.rules
+sudo systemctl restart udev
+echo "..."
+echo "..."
+echo "..."
+echo "Please relogin or restart computer and replugin launchpad !!!"
+echo "..."
